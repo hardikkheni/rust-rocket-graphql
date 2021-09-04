61a12e3efcb9983f233ac8c2098a7fa5c06226bd
HardikKheni <hardikkheni.work@gmail.com>
2021-09-04 12:41:44 +0530

    mutation added and query improved.
    1). "InsertProduct" mutation added.
    2). "products" query improved by adding all field in "Product" types.

--- src/db/models.rs
+++ src/db/models.rs
@@ -1,6 +1,7 @@
+use super::schema::products;
 use chrono::NaiveDateTime;
-use diesel::Queryable;
-use juniper::graphql_object;
+use diesel::{Insertable, Queryable};
+use juniper::{graphql_object, GraphQLInputObject};
 
 #[derive(Queryable)]
 pub struct Product {
@@ -19,7 +20,36 @@ impl Product {
     fn id(&self) -> i32 {
         self.id
     }
+    #[graphql(name = "product_id")]
+    pub fn product_id(&self) -> &str {
+        self.product_id.as_str()
+    }
+    #[graphql(name = "garment_style")]
+    pub fn garment_style(&self) -> &str {
+        self.garment_style.as_str()
+    }
+    #[graphql(name = "garment_color")]
+    pub fn garment_color(&self) -> &str {
+        self.garment_style.as_str()
+    }
+    #[graphql(name = "garment_size")]
+    pub fn garment_size(&self) -> &str {
+        self.garment_size.as_str()
+    }
     pub fn quantity(&self) -> i32 {
         self.quantity
     }
+}
+
+#[derive(GraphQLInputObject, Insertable)]
+#[table_name = "products"]
+pub struct InsertProductInput {
+    #[graphql(name = "product_id")]
+    pub product_id: String,
+    #[graphql(name = "garment_style")]
+    pub garment_style: String,
+    #[graphql(name = "garment_color")]
+    pub garment_color: String,
+    #[graphql(name = "garment_size")]
+    pub garment_size: String,
 }
